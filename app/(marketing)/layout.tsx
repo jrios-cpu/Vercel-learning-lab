ec75435f928377468fe3cf37cac7e006bbbbf63c
jrios-cpu <jrios@tekulus.com>
2026-09-03 16:58:35 -0600

    feat: add RXL marketing layout

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,5 @@
+import "../rxl-marketing.css";
+
+export default function MarketingLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return <div className="rxl-marketing">{children}</div>;
+}
